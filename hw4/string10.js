c3d3ed72c90a362de12b4d1dc82b00d3119ce06a
skrav4enko <s.kravchenko@outlook.com>
2018-03-26 21:00:08 +0300

    hw4

---
+++ hw4/string10.js
@@ -0,0 +1,13 @@
+var string;
+
+string = prompt('Введите строку', 'Hello world');
+
+if (string === '') {
+  console.log('Вы ничего не ввели. Строка пуста.')
+} else if (string.length < 10) {
+  for (let i = 0; i < string.length; i++) {
+    console.log(string[i]);
+  }
+} else {
+  console.log(string.substring(0,7) + string.substr(10));
+}
