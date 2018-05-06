9858c9de0f5f9eb5c9e05142fdbd05411aa5290b
skrav4enko <s.kravchenko@outlook.com>
2018-05-06 13:36:34 +0300

    upfdate functions

--- hw5/checknumber.js
+++ hw5/checknumber.js
@@ -1,42 +1,58 @@
-// Вывод результата в консоль
-console.log(checkNumber(197));
+let number = 3;
 
+// Проверка результата в консоль
+checkNumber(number);
+
+// Функция проверки числа
 function checkNumber(number) {
   // Создаем массив для сохранение результатов проверки
-  let result = [];
   
+  let resultTotal = [isPrime(number), isEven(number), isMultipleOf10(number)];
+
   // Проверка на простое число
-  if (number < 2){
-    result.push(false);
-  } else {
-    for (let i = 2; i <= Math.sqrt(number); i++) {
-      if (number % i === 0) {
-        result.push(false); 
-        break;
-      }
-      else {
-        result.push(true); 
-        break;
+  function isPrime(number) {
+    let result;
+
+    if (number < 2){
+      result = false;
+    } else if (number == 2 || number == 3){
+      result = true;
+    } else {
+      for (let i = 2; i <= Math.sqrt(number); i++) {
+        if (number % i == 0) {
+          result = false; break;
+        }
+        else {
+          result = true;
+        }
       }
     }
+    return result;
   }
-  
+
   // Проверка на четность
-  if (number % 2 === 0 ){
-    result.push(true);
+  function isEven(number) {
+    let result;
+    if (number % 2 == 0 ){
+      result = true;
+    } else{
+      result = false;
     }
-  else{
-    result.push(false);
+    return result;
   }
 
   // Проверка на кратность 10
-  if (number % 10 === 0 ){
-    result.push(true);
+  function isMultipleOf10(number) {
+    let result;
+    if (number % 10 == 0 ){
+      result = true;
+    } else{
+      result = false;
     }
-  else{
-    result.push(false);
+    return result;
   }
 
   // Возврат результата проверки
-  return result;
+  return console.log(resultTotal);
+  // return console.log(`Number ${number} prime/even/multiplerOf10 - ${resultTotal}`);
 }
--- hw5/validatepass.js
+++ hw5/validatepass.js
@@ -1,35 +1,60 @@
 var password = 'TcSf12y';
 
-function validatePassword(pass) {
-  // Инициализация переменной для результата функции
-  let result;
-  
-  // console.log(pass + ' length: ' + pass.length); // проверочный вывод строки и ее длины
-  
-  // Начальная проверка пароля на длину
-  if (pass.length >= 6 && pass.length <= 20) {
-    // РАБОТАЕТ! Проверка пароля регулярным выражением
-    // let reg = /^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]+$/;
-    // reg.test(pass) 
-    //   ? result = true 
-    //   : result = false;
-    
-    // Проверка пароля посимвольно ..запутаный вариант
-    for (let i = 0; i < pass.length; i++) {
-      let e = pass.charCodeAt(i);
-      if (e < 48 || e > 57 && e < 65 || e > 90 && e < 97 || e > 122 ) {
-        result =  false; // Если хоть один символ не цифра ил буква, то false
-        break;  // Прерывание работы цикла при первом false
-      } else {
-        result =  true;
-      }
-    }
-  } else {
-    result = false; // Если пароль не прошел по длине
-  }
-  return result;  // Возврат результата выполнения функции
-}
-
 validatePassword(password) 
   ? console.log('VALID') 
   : console.log('INVALID');
+
+function validatePassword(pass) {
+    
+  console.log(pass + ' length: ' + pass.length); // проверочный вывод строки и ее длины
+  
+  function checkPassLength(pass) {
+    let result;
+    if (pass.length >= 6 && pass.length <= 20) {
+      result = true;
+    } else {
+      result = false;
+    }
+    return result;
+  }
+
+  function checkPassAlphanumeric(pass) {
+    let result1;
+    let result2;
+    let result3;
+
+    for (let i = 0; i < pass.length; i++) {
+      let e = pass.charCodeAt(i);
+      if (e < 48 || e > 57 && e < 65 || e > 90 && e < 97 || e > 122 ) {
+        result1 =  false; break;
+      } else {
+        result1 =  true;
+      }
+    }
+
+    for (let i = 0; i < pass.length; i++) {
+      let e = pass.charCodeAt(i);
+      if (e < 48 || e > 57) {
+        result2 =  false; 
+      } else {
+        result2 =  true; break;
+      }
+    }
+
+    for (let i = 0; i < pass.length; i++) {
+      let e = pass.charCodeAt(i);
+      if (e < 65 || e > 90 && e < 97 || e > 122 ) {
+        result3 =  false; 
+      } else {
+        result3 =  true; break;
+      }
+    }
+    return (result1 && result2 && result3)
+      ? true 
+      : false;
+  }
+  
+  return (checkPassLength(pass) && checkPassAlphanumeric(pass))
+    ? true
+    : false;
+}
