37f9da0d79daf11fe4bb5d1fb947970b4f9976f6
skrav4enko <s.kravchenko@outlook.com>
2018-05-08 00:55:23 +0300

    settimeout 2 variant

--- hw10/js/settimeout.js
+++ hw10/js/settimeout.js
@@ -8,9 +8,17 @@
 for(let i = 1; i < 11; i++) {
   count(i);
 }
-
 function count(i) {
   setTimeout(function() {
     console.log(i);
-  }, 1000);
+  }, i * 100);
+}
+
+// Solution 2
+for(var i = 1; i < 11; i++) {
+  (function (i) {
+    setTimeout(function() {
+      console.log(i);
+    }, i * 100)
+  }(i));
 }
