2ea72f45e929deb1023f138651beb5ad762e03ab
skrav4enko <s.kravchenko@outlook.com>
2018-05-08 12:51:13 +0300

    timer

--- hw14/js/date.js
+++ hw14/js/date.js
@@ -2,8 +2,12 @@
 document.addEventListener('DOMContentLoaded', () => {
   
   const clockArea = document.querySelector('#clock-area');
+  const timerArea = document.querySelector('#timer-area');
+
+  let date = new Date();
   
   clock();
+  // spentTime();
 
   function clock() {
     let time = new Date();
@@ -17,6 +21,30 @@ document.addEventListener('DOMContentLoaded', () => {
     
     function showClock() {
       clockArea.innerHTML = `${format(hours)}: ${format(minutes)}: ${format(seconds)}`;
+    }
+
+    function format(number) {
+      if (number < 10) {
+        number = '0' + number;
+      }
+      return number;
+    }
+  }
+
+  function spentTime() {
+    let currentDate = new Date();
+
+    let spentTime = currentDate - date;
+    let hours = spentTime.getHours();
+    let minutes = spentTime.getMinutes();
+    let seconds = spentTime.getSeconds();
+
+    setTimeout(spentTime, 1000);
+    
+    showClock();
+    
+    function showClock() {
+      timerArea.innerHTML = `You spent ${format(hours)}: ${format(minutes)}: ${format(seconds)} on this site`;
     }
 
     function format(number) {
