439511079da6016b2af62f17f0db44b4d38e50a4
skrav4enko <s.kravchenko@outlook.com>
2018-05-09 21:46:34 +0300

    change logo and toggle menu works

--- layout/js/script.js
+++ layout/js/script.js
@@ -1,19 +1,33 @@
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+document.addEventListener('DOMContentLoaded', () => {
+    
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function() {scrollFunction();};
 
-function scrollFunction() {
+  function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("myBtn").style.display = "block";
+      document.getElementById('myBtn').style.display = 'block';
     } else {
-        document.getElementById("myBtn").style.display = "none";
+      document.getElementById('myBtn').style.display = 'none';
     }
-}
+  }
 
+
+
+  let toggleMenu = document.querySelector('#toggleMenu');
+  let toggleMenuStatus = 1;
+
+  toggleMenu.addEventListener('click', () => {
+    if (toggleMenuStatus == 1) {
+      document.querySelector('.nav_header').classList.add('showMenu');
+      toggleMenuStatus = 0;
+    } else {
+      document.querySelector('.nav_header').classList.remove('showMenu');
+      toggleMenuStatus = 1;
+    }
+  });
+});
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
-
-let nav_button = document.querySelector('#menu_button');
-nav_button.addEventListener('click', () => {document.querySelector('.sidebar').style.display = 'block'});
