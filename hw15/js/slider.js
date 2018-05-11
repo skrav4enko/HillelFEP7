434436c1977cd8feb305b836c5ddc661a3d09b26
skrav4enko <s.kravchenko@outlook.com>
2018-05-11 16:22:59 +0300

    slider

--- hw15/js/slider.js
+++ hw15/js/slider.js
@@ -1,1 +1,38 @@
-document.querySelector('.carousel');
+
+let arrowLeft = document.querySelector('#prevArrow');
+let arrowRight = document.querySelector('#nextArrow');
+
+let slides = document.querySelectorAll('.carousel-item');
+let dots = document.querySelectorAll('dot');
+
+let slideIndex = 1;
+
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  } 
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none'; 
+  }
+  for (let i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(' active', '');
+  }
+  slides[slideIndex-1].style.display = 'block'; 
+  dots[slideIndex-1].className += ' active';
+}
