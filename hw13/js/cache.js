94fb1da7ca1c890cc214bf8fc7b23f0e2e6b08cf
skrav4enko <s.kravchenko@outlook.com>
2018-05-11 12:43:12 +0300

    cache function

--- hw13/js/cache.js
+++ hw13/js/cache.js
@@ -1,11 +1,34 @@
-var complexFunction = function(arg1, arg2) { 
-  return arg1 + arg2;
+let cache = {};
+let i = 0;
+
+var complexFunction = function (arg1, arg2) {
+  let result =  arg1 + arg2;
+  let key = i;
+  cache[key] = {};
+  cache[key]['arg1'] = arg1;
+  cache[key]['arg2'] = arg2;
+  cache[key]['result'] = result;
+  i++;
+
+  return console.log(`Result ${result} insert to cache for future`);
 };
-var cachedFunction = function cache(complexFunction) {
-  let cache = [];
-  return complexFunction()
+
+var cachedFunction = function (arg1, arg2) {
+  for (let key in cache) {
+    if (cache[key].arg1 === arg1 && cache[key].arg2 === arg2) {
+      return console.log(`Result ${cache[key].result} from cache`);
+    }
+  }
+
+  return complexFunction(arg1, arg2)
 };
 
 cachedFunction('foo', 'bar');
 cachedFunction('foo', 'bar');
 cachedFunction('foo', 'baz');
+cachedFunction('foo', 'bor');
+cachedFunction('hoo', 'boo');
+cachedFunction('foo', 'baz');
+cachedFunction('hoo', 'boo');
+
+console.log(cache);
