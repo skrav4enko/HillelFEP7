307a6da2a7d009c5f86f710447b59c4f6fdaed68
skrav4enko <s.kravchenko@outlook.com>
2018-05-20 23:54:46 +0300

    slideshow

--- hwlayout/js/slider.js
+++ hwlayout/js/slider.js
@@ -13,10 +13,8 @@ document.addEventListener('DOMContentLoaded', () => {
       mainSection.removeChild(mainSection.firstChild); // удалить при обнаружении
     }
 
-    let templateTable = `<h2>Infinite slider</h2>
+    let templateTable = `<h2>Infinite Slideshow</h2>
     <div class="slideshow-container">
-
-      <!-- Full-width images with number and caption text -->
       <div class="carousel-item active">
         <img class="d-block w-100" src="./img/pexels-photo-01.jpeg" alt="First slide">
       </div>
@@ -32,57 +30,19 @@ document.addEventListener('DOMContentLoaded', () => {
       <div class="carousel-item">
         <img class="w-100" src="./img/pexels-photo-05.jpeg" alt="Fifth slide">
       </div>
-
-      <!-- Next and previous buttons -->
-      <a id="prevArrow" class="prev" onclick="plusSlides(-1)">&#10094;</a>
-      <a id="nextArrow" class="next" onclick="plusSlides(1)">&#10095;</a>
-    </div>
-    <!-- The dots/circles -->
-    <div style="text-align:center">
-      <span class="dot active" onclick="currentSlide(1)"></span> 
-      <span class="dot" onclick="currentSlide(2)"></span> 
-      <span class="dot" onclick="currentSlide(3)"></span> 
     </div>`;
 
     mainSection.innerHTML = templateTable;
-  }
+    let slides = document.querySelectorAll('.carousel-item');
+    let currentSlide = 0;
+    let slideInterval = setInterval(nextSlide, 3000);
+    // console.log(slides);
 
-  let arrowLeft = document.querySelector('#prevArrow');
-  let arrowRight = document.querySelector('#nextArrow');
-  
-  let slides = document.querySelectorAll('.carousel-item');
-  let dots = document.querySelectorAll('dot');
-  
-  let slideIndex = 1;
-  
-  showSlides(slideIndex);
-  
-  // Next/previous controls
-  function plusSlides(n) {
-    showSlides(slideIndex += n);
-  }
-  
-  // Thumbnail image controls
-  function currentSlide(n) {
-    showSlides(slideIndex = n);
-  }
-  
-  function showSlides(n) {
-  
-    // if (n > slides.length) {
-    //   slideIndex = 1;
-    // } 
-    // if (n < 1) {
-    //   slideIndex = slides.length;
-    // }
-    // for (let i = 0; i < slides.length; i++) {
-    //   slides[i].style.display = 'none'; 
-    // }
-    // for (let i = 0; i < dots.length; i++) {
-    //   dots[i].className = dots[i].className.replace(' active', '');
-    // }
-    // slides[slideIndex-1].style.display = 'block'; 
-    // dots[slideIndex-1].className += ' active';
+    function nextSlide() {
+      slides[currentSlide].classList.remove('active');
+      currentSlide = (currentSlide + 1) % slides.length;
+      slides[currentSlide].classList.add('active');
+    }
   }
 });
 
