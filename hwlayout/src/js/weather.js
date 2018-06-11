d2f42dd6f09948c1defde9d78a8def963d477405
skrav4enko <s.kravchenko@outlook.com>
2018-06-11 20:07:58 +0300

    weather fix

--- hwlayout/src/js/weather.js
+++ hwlayout/src/js/weather.js
@@ -3,23 +3,42 @@ function GetForecast() {
 
   const cityInput = document.querySelector('#searchCity');
   const autocomplete = new google.maps.places.Autocomplete(cityInput);
+  autocomplete.addListener('place_changed', () => {});
   // console.log(autocomplete);
   // https://maps.googleapis.com/maps/api/place/autocomplete/json?input=input&types=geocode&key=API_KEY
 
   /* ---WEATHER--- */
+  
 
   const wbtn = document.querySelector('#getCity');
   const forecastHeader = document.querySelector('#forecast-header');
   wbtn.addEventListener('click', fetchWeatherData);
 
-  google.maps.event.addListener(autocomplete, 'place_changed', () => {});
+  const apiCnf = {
+    baseUrl: 'http://api.openweathermap.org/data/2.5/weather',
+    appId: 'be69c9c340c7433ef933d94b2c2db766',
+    query: 'units=metric&lang=ru'
+  };
 
-  const baseUrl = 'http://api.openweathermap.org/data/2.5/weather';
-  const appId = 'be69c9c340c7433ef933d94b2c2db766';
-  const query = `units=metric&lang=ru&appid=${appId}`;
+  let cityInfo = {};
+  function load() {
+    if (localStorage.cityInfo) {
+      cityInfo = JSON.parse(localStorage.getItem('cityInfo'));
+      console.log(cityInfo);
+      fetchWeatherData();
+    } else {
+      forecastHeader.innerHTML =
+      `Узнать погоду <i class="far fa-hand-point-right"></i>`;
+    }
+  }
+  load();
+
+  // const baseUrl = 'http://api.openweathermap.org/data/2.5/weather';
+  // const appId = 'be69c9c340c7433ef933d94b2c2db766';
+  // const query = `units=metric&lang=ru&appid=${appId}`;
 
   function weatherIcon(time, weatherCode) {
-    const timeOfDay = (time > 7 && time < 18) ? 'day' : 'night';
+    const timeOfDay = time > 7 && time < 18 ? 'day' : 'night';
     const className = `weather-icon wi wi-owm-${timeOfDay}-${weatherCode}`;
     const icon = `<i class="${className}"></i>`;
 
@@ -27,14 +46,17 @@ function GetForecast() {
   }
 
   function fetchWeatherData(event) {
-    event.preventDefault();
-    const placeName = autocomplete.getPlace().name;
-    const lat = autocomplete.getPlace().geometry.location.lat();
-    const lng = autocomplete.getPlace().geometry.location.lng();
-    // console.log(autocomplete.getPlace());
+    cityInfo.placeName = autocomplete.getPlace().name;
+    cityInfo.lat = autocomplete.getPlace().geometry.location.lat();
+    cityInfo.lng = autocomplete.getPlace().geometry.location.lng();
 
-    const url = `${baseUrl}?lat=${lat}&lon=${lng}&${query}`;
-    // console.log(url);
+    localStorage.setItem('cityInfo', JSON.stringify(cityInfo));
+
+    const url = `${apiCnf.baseUrl}?lat=${cityInfo.lat}&lon=${cityInfo.lng}&${
+      apiCnf.query
+    }&appid=${apiCnf.appId}`;
+    event.preventDefault();
+    console.log(url);
 
     fetch(url)
       .then(response => response.json())
@@ -42,7 +64,12 @@ function GetForecast() {
         const date = new Date();
         const time = date.getHours();
 
-        forecastHeader.innerHTML = `<span>${placeName} </span><span title="${data.weather[0].description}"> ${weatherIcon(time, data.weather[0].id)} </span><span> ${Math.round(data.main.temp)}&deg; С</span>`;
+        forecastHeader.innerHTML = `<span>${
+          cityInfo.placeName
+        } </span><span title="${data.weather[0].description}"> ${weatherIcon(
+          time,
+          data.weather[0].id
+        )} </span><span> ${Math.round(data.main.temp)}&deg; С</span>`;
         cityInput.value = '';
         // console.log(data);
       })
